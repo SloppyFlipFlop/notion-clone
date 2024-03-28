969e0bbc3f30bc576d6aa387bf35a9bbaec62e45
David Nevarez <69279785+SloppyFlipFlop@users.noreply.github.com>
2024-03-27 22:31:58 -0700

    Feature: Added button to allow users to create notes.

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -4,9 +4,27 @@ import Image from 'next/image';
 import { useUser } from '@clerk/clerk-react';
 import { Button } from '@/components/ui/button';
 import { PlusCircle } from 'lucide-react';
+import { useMutation } from 'convex/react';
+import { api } from '@/convex/_generated/api';
+import { Toaster, toast } from 'sonner';
 
 const Documents = () => {
   const { user } = useUser();
+
+  const create = useMutation(api.documents.create);
+
+  const handleCreateDocument = async () => {
+    const promise = create({
+      title: 'Untitled Document',
+    });
+
+    toast.promise(promise, {
+      loading: 'Creating note...',
+      success: 'Note created!',
+      error: 'Failed to create new note. Please try again.',
+    });
+  };
+
   return (
     <div className='h-full flex flex-col items-center justify-center space-y-4'>
       <Image
@@ -26,7 +44,7 @@ const Documents = () => {
 
       <h2>Welcome to {user?.firstName}&apos;s Jotion</h2>
 
-      <Button>
+      <Button onClick={handleCreateDocument}>
         <PlusCircle className='h-4 w-4 mr-2' />
         Create a Note
       </Button>

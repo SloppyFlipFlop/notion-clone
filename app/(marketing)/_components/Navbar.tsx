60946b9271a133a5bc2254bf74e6cb049c6e4646
David Nevarez <69279785+SloppyFlipFlop@users.noreply.github.com>
2024-03-27 18:05:23 -0700

    Feature: Added authentication to component.

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -4,9 +4,16 @@ import { useScrollTop } from '@/hooks/use-scroll-top';
 import { cn } from '@/lib/utils';
 import Logo from './Logo';
 import { ModeToggle } from '@/components/mode-toggle';
+import { useConvexAuth } from 'convex/react';
+import { SignInButton, UserButton } from '@clerk/clerk-react';
+import { Button } from '@/components/ui/button';
+import { Spinner } from '@/components/spinner';
+import Link from 'next/link';
 
 const Navbar = () => {
   const scrolled = useScrollTop();
+
+  const { isAuthenticated, isLoading } = useConvexAuth();
 
   return (
     <div
@@ -17,6 +24,29 @@ const Navbar = () => {
     >
       <Logo />
       <div className='md:lg-auto md:justify-end justify-between w-full flex items-center gap-x-2'>
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode='modal'>
+              <Button variant={'ghost'} size={'sm'}>
+                Login
+              </Button>
+            </SignInButton>
+            <SignInButton mode='modal'>
+              <Button variant={'ghost'} size={'sm'}>
+                Get Jotion Free
+              </Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant={'ghost'} size={'sm'}>
+              <Link href='/documents'>Enter Jotion</Link>
+            </Button>
+            <UserButton afterSignOutUrl='/' />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>

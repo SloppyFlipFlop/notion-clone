bbee2f4939e776195305d5887e1b61189c9afa71
David Nevarez <69279785+SloppyFlipFlop@users.noreply.github.com>
2024-03-28 10:48:59 -0700

    Fix: Fixed formatting.

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,10 +1,12 @@
 'use client';
 
-import { Spinner } from '@/components/spinner';
 import { useConvexAuth } from 'convex/react';
 import { redirect } from 'next/navigation';
-import { Navigation } from './_components/navigation';
+
+import { Spinner } from '@/components/spinner';
 import { SearchCommand } from '@/components/search-command';
+
+import { Navigation } from './_components/navigation';
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   const { isAuthenticated, isLoading } = useConvexAuth();
@@ -12,7 +14,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
   if (isLoading) {
     return (
       <div className='h-full flex items-center justify-center'>
-        <Spinner size={'lg'} />
+        <Spinner size='lg' />
       </div>
     );
   }
@@ -22,7 +24,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
   }
 
   return (
-    <div className='h-full flex dark:bg-[#1f1f1f]'>
+    <div className='h-full flex dark:bg-[#1F1F1F]'>
       <Navigation />
       <main className='flex-1 h-full overflow-y-auto'>
         <SearchCommand />

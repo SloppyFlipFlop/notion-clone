5d3d55e2dde3eb9cd73080e44b1f9cc92a94aaa4
David Nevarez <69279785+SloppyFlipFlop@users.noreply.github.com>
2024-03-27 21:44:44 -0700

    Feature: Created landing document page.

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,5 +1,37 @@
+'use client';
+
+import Image from 'next/image';
+import { useUser } from '@clerk/clerk-react';
+import { Button } from '@/components/ui/button';
+import { PlusCircle } from 'lucide-react';
+
 const Documents = () => {
-  return <div>This is a protected document</div>;
+  const { user } = useUser();
+  return (
+    <div className='h-full flex flex-col items-center justify-center space-y-4'>
+      <Image
+        src='/empty.png'
+        alt='Empty '
+        width={300}
+        height={300}
+        className='dark:hidden'
+      />
+      <Image
+        src='/empty-dark.png'
+        alt='Empty'
+        width={300}
+        height={300}
+        className='hidden dark:block'
+      />
+
+      <h2>Welcome to {user?.firstName}&apos;s Jotion</h2>
+
+      <Button>
+        <PlusCircle className='h-4 w-4 mr-2' />
+        Create a Note
+      </Button>
+    </div>
+  );
 };
 
 export default Documents;

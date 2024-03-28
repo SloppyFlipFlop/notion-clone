6f4e6938e2ba82546777359b54ea863e2bd0600d
David Nevarez <69279785+SloppyFlipFlop@users.noreply.github.com>
2024-03-27 22:31:15 -0700

    Feature: Added ability to receive notifications when a note is created successfully, unsuccessfully, and for error messages.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import './globals.css';
 import { ThemeProvider } from '@/components/providers/theme-provider';
 import { ConvexClientProvider } from '@/components/providers/convex-provider';
 
+import { Toaster } from 'sonner';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -40,6 +42,7 @@ export default function RootLayout({
             disableTransitionOnChange
             storageKey='jotion-theme'
           >
+            <Toaster position='bottom-center' />
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
